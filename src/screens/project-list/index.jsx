e85db6c2c1d24d0f5f6250dd9223703625f5a26b
杨铭 <myang@landhightech.com>
2021-07-22 14:58:36 +0800

    [dev] 代码提交

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import { SearchPanel } from "./search-panel";
+import { List } from "./list";
+import { cleanObject } from "../../utils";
+import * as qs from "qs";
+const apiUrl = process.env.REACT_APP_API_URL;
+// const apiUrl = "http://localhost:3001";
+console.log(process.env.REACT_APP_API_URL);
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const [users, setUsers] = useState([]);
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
+      async (response) => {
+        if (response.ok) {
+          setList(await response.json());
+        }
+      }
+    );
+  }, [param]);
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  }, []);
+  return (
+    <div>
+      <SearchPanel
+        users={users}
+        param={param}
+        setParam={setParam}
+      ></SearchPanel>
+      <List users={users} list={list}></List>
+    </div>
+  );
+};
